30df41a9d63e076626edd1af01d0d314f2914379
EvanMevel <evan.mevel@epitech.eu>
2023-03-02 20:56:57 +0100

    FEAT added button to add folders

--- app/src/main/java/fr/emevel/locallink/locallink_android/FirstFragment.java
+++ app/src/main/java/fr/emevel/locallink/locallink_android/FirstFragment.java
@@ -7,6 +7,9 @@ import android.provider.DocumentsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -14,9 +17,13 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.io.File;
+
 import fr.emevel.locallink.locallink_android.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
+
+    public static FirstFragment instance;
 
     private FragmentFirstBinding binding;
 
@@ -33,8 +40,14 @@ public class FirstFragment extends Fragment {
                     String path = PathUtils.getPath(getActivity(), docUri);
 
                     System.out.println(path);
+
+                    File f = new File(path);
+
+                    instance.add(f);
                 }
             });
+
+    private LinearLayout layout;
 
     @Override
     public View onCreateView(
@@ -42,7 +55,12 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
+        instance = this;
+
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        layout = binding.container;
+
         return binding.getRoot();
     }
 
@@ -60,6 +78,21 @@ public class FirstFragment extends Fragment {
                 //NavHostFragment.findNavController(FirstFragment.this).navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+    }
+
+    public void add(File file) {
+        View view = getLayoutInflater().inflate(R.layout.folder, null);
+        TextView text = view.findViewById(R.id.name);
+        text.setText(file.getAbsolutePath());
+        ImageView image = view.findViewById(R.id.logo);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+
+        layout.addView(view);
     }
 
     @Override
